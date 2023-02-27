111da9e3e7d21703ac927e3f718edcaf950ecbae
Barni072 <93314898+Barni072@users.noreply.github.com>
2023-02-27 16:23:49 +0100

    Affichage - TIPE 27/02/23

--- affichage.h
+++ affichage.h
@@ -2,7 +2,7 @@
 #define GRILLE_H
 
 void init_ncurses();
-void dessine_case(int x, int y, int couleur,char c);
+//void dessine_case(int x, int y, int couleur,char c);
 void affiche(grille* g);
 
 #endif
